8e3fe4bed7926d7723f57a87a9400974b50b170e
ompatel616 <omjpatel6461@gmail.com>
2025-10-05 11:04:45 +0530

    fix(backend setup): add basic setup of firebase and env configuration

---
+++ apps/invoicely-backend/src/app/utils/constants.ts
@@ -0,0 +1,4 @@
+export enum FIREBASE {
+  APP = 'APP',
+  AUTH = 'AUTH',
+}
--- apps/invoicely-backend/src/main.ts
+++ apps/invoicely-backend/src/main.ts
@@ -5,17 +5,18 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function startServer() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 5000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
-bootstrap();
+startServer();
