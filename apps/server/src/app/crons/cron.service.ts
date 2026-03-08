75e2d0d67c7119680356aca14914612fd1cf5fa5
ompatel616 <omjpatel6461@gmail.com>
2026-03-08 15:39:10 +0530

    fix(server): make 1 min cron time to not inactive render instance

--- apps/server/src/app/crons/cron.service.ts
+++ apps/server/src/app/crons/cron.service.ts
@@ -6,7 +6,7 @@ import { Cron } from '@nestjs/schedule';
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
-  @Cron('*/10 * * * *')
+  @Cron('*/1 * * * *')
   async handleCron() {
     this.logger.log('Automatic Task running');
     await new Promise((resolve) => {
